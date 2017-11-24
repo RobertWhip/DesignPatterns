ecabde00255cdb030bbad1109f8c336f0a332d43
RobertWhip <il.cint@mail.ru>
2017-11-24 18:58:07 +0300

    Add files via upload
    Main -> MiniDuckSimulator.java

---
+++ Strategy/MiniDuckSimulator.java
@@ -0,0 +1,17 @@
+public class MiniDuckSimulator {
+	public static void main(String... args) {
+		Duck mallard = new MallardDuck();
+		mallard.performQuack();
+		mallard.performFly();
+		mallard.swim();
+		mallard.display();
+		
+		System.out.println();
+		
+		Duck model = new ModelDuck();
+		model.performFly();
+		model.setFlyBehavior(new FlyRocketPowered());
+		model.performFly();
+		model.display();
+	}
+}
